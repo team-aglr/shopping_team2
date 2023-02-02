0ee7faad1b69fe85ef7ee0ddc2cffe86a6e93878
Luke Oguro <lukeoguro@gmail.com>
2023-02-02 14:25:14 -0500

    Fix cart totals

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,7 +1,7 @@
 import CartItem from "./CartItem";
 
 const Cart = ({ cartItems, onCheckout }) => {
-  const total = cartItems.reduce((acc, item) => acc + item.price, 0)
+  const total = cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0)
 
   const handleCheckout = (e) => {
     e.preventDefault();
@@ -11,21 +11,29 @@ const Cart = ({ cartItems, onCheckout }) => {
   return (
     <div className="cart">
       <h2>Your Cart</h2>
-      <table className="cart-items">
-        <tbody><tr>
-          <th>Item</th>
-          <th>Quantity</th>
-          <th>Price</th>
-        </tr>
-          {cartItems.map(item => (
-            <CartItem item={item} key={item._id} />
-          ))}
-          <tr>
-            <td colSpan="3" className="total">Total: ${total.toFixed(2)}</td>
+      {cartItems.length === 0 ?
+        <>
+          <p>Your cart is empty</p>
+          <p>Total: $0</p>
+        </> :
+        <table className="cart-items">
+          <tbody><tr>
+            <th>Item</th>
+            <th>Quantity</th>
+            <th>Price</th>
           </tr>
-        </tbody></table>
-      <a className="button checkout" onClick={handleCheckout}>Checkout</a>
-    </div>
+            {cartItems.map(item => (
+              <CartItem item={item} key={item._id} />
+            ))}
+            <tr>
+              <td colSpan="3" className="total">Total: ${total.toFixed(2)}</td>
+            </tr>
+          </tbody>
+        </table>
+      }
+      <a className={`button checkout ${cartItems.length === 0 ? "disabled" : ""}`}
+        onClick={handleCheckout}>Checkout</a>
+    </div >
   )
 }
 
